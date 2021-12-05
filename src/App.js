e2ff379d86537ce4d102f58cac0f87bc8a888089
yohanessahrul <yohanessahrul92@gmail.com>
2021-12-05 12:03:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
